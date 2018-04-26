a31629dda539212eb6840fbad21e136b01bfba9e
Rafael Kaciano Wolf <rafakwolf@outlook.com>
2018-04-25 21:20:55 -0300

    disable default security for now + CadastroPJController

--- src/main/java/com/rafakwolf/pontointeligente/api/controllers/CadastroPJController.java
+++ src/main/java/com/rafakwolf/pontointeligente/api/controllers/CadastroPJController.java
@@ -1,22 +1,32 @@
 package com.rafakwolf.pontointeligente.api.controllers;
 
 import com.rafakwolf.pontointeligente.api.dtos.CadastroPJDto;
+import com.rafakwolf.pontointeligente.api.entities.Empresa;
+import com.rafakwolf.pontointeligente.api.entities.Funcionario;
+import com.rafakwolf.pontointeligente.api.enums.PerfilEnum;
 import com.rafakwolf.pontointeligente.api.response.Response;
 import com.rafakwolf.pontointeligente.api.services.EmpresaService;
 import com.rafakwolf.pontointeligente.api.services.FuncionarioService;
+import com.rafakwolf.pontointeligente.api.utils.PasswordUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.security.NoSuchAlgorithmException;
 
 @RestController
 @RequestMapping("/api/cadastrar-pj")
 @CrossOrigin(origins = "*")
 public class CadastroPJController {
+
+    private static final Logger log = LoggerFactory.getLogger(CadastroPJController.class);
 
     private FuncionarioService funcionarioService;
     private EmpresaService empresaService;
@@ -27,7 +37,69 @@ public class CadastroPJController {
     }
 
     public ResponseEntity<Response<CadastroPJDto>> cadastrar(@Valid @RequestBody CadastroPJDto cadastroPJDto,
-                                                             BindingResult result) {
-        return null;
+                                                             BindingResult result) throws NoSuchAlgorithmException {
+        log.info("Cadastrando PJ: {}", cadastroPJDto.toString());
+        Response<CadastroPJDto> response = new Response<>();
+
+        validarDadosExistentes(cadastroPJDto, result);
+
+        Empresa empresa = this.converterDtoParaEmpresa(cadastroPJDto);
+        Funcionario funcionario = this.converterDtoParaFuncionario(cadastroPJDto);
+
+        if (result.hasErrors()) {
+            log.error("Erro validando dados de cadastro PJ: {}", result.getAllErrors());
+            result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+            return ResponseEntity.badRequest().body(response);
+        }
+
+        this.empresaService.persistir(empresa);
+        funcionario.setEmpresa(empresa);
+        this.funcionarioService.persistir(funcionario);
+
+        response.setData(this.converterCadastroPJDto(funcionario));
+        return ResponseEntity.ok(response);
+    }
+
+    private void validarDadosExistentes(CadastroPJDto cadastroPJDto, BindingResult result) {
+        this.empresaService.buscarPorCnpj(cadastroPJDto.getCnpj())
+                .ifPresent(emp -> result.addError(new ObjectError("empresa", "Empresa já existente.")));
+
+        this.funcionarioService.buscarPorCpf(cadastroPJDto.getCpf())
+                .ifPresent(func -> result.addError(new ObjectError("funcionario", "CPF já existente.")));
+
+        this.funcionarioService.buscarPorEmail(cadastroPJDto.getEmail())
+                .ifPresent(func -> result.addError(new ObjectError("funcionario", "Email já existente.")));
+    }
+
+    private Empresa converterDtoParaEmpresa(CadastroPJDto cadastroPJDto) {
+        Empresa empresa = new Empresa();
+        empresa.setCnpj(cadastroPJDto.getCnpj());
+        empresa.setRazaoSocial(cadastroPJDto.getRazaoSocial());
+
+        return empresa;
+    }
+
+    private Funcionario converterDtoParaFuncionario(CadastroPJDto cadastroPJDto)
+            throws NoSuchAlgorithmException {
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome(cadastroPJDto.getNome());
+        funcionario.setEmail(cadastroPJDto.getEmail());
+        funcionario.setCpf(cadastroPJDto.getCpf());
+        funcionario.setPerfil(PerfilEnum.ROLE_ADMIN);
+        funcionario.setSenha(PasswordUtils.gerarBCrypt(cadastroPJDto.getSenha()));
+
+        return funcionario;
+    }
+
+    private CadastroPJDto converterCadastroPJDto(Funcionario funcionario) {
+        CadastroPJDto cadastroPJDto = new CadastroPJDto();
+        cadastroPJDto.setId(funcionario.getId());
+        cadastroPJDto.setNome(funcionario.getNome());
+        cadastroPJDto.setEmail(funcionario.getEmail());
+        cadastroPJDto.setCpf(funcionario.getCpf());
+        cadastroPJDto.setRazaoSocial(funcionario.getEmpresa().getRazaoSocial());
+        cadastroPJDto.setCnpj(funcionario.getEmpresa().getCnpj());
+
+        return cadastroPJDto;
     }
 }

086d72869c9671fb441beaae41ce3c99d58cd461
hbsis-rafaelwolf <rafael.wolf@hbsis.com.br>
2018-05-02 15:53:22 -0300

    ajuste deprecated libs ref

--- src/main/java/com/rafakwolf/pontointeligente/api/security/dto/JwtAuthenticationDto.java
+++ src/main/java/com/rafakwolf/pontointeligente/api/security/dto/JwtAuthenticationDto.java
@@ -1,7 +1,7 @@
 package com.rafakwolf.pontointeligente.api.security.dto;
 
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 
 public class JwtAuthenticationDto {
 	
